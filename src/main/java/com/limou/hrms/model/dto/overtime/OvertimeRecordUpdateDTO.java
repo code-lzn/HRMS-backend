d8a0200961c2942548018343dbe7f8da4a4a5c19
zhanghao <2571761868@qq.com>
2026-07-23 12:57:00 +0800

    更新

--- src/main/java/com/limou/hrms/model/dto/overtime/OvertimeRecordUpdateDTO.java
+++ src/main/java/com/limou/hrms/model/dto/overtime/OvertimeRecordUpdateDTO.java
@@ -19,6 +19,7 @@ public class OvertimeRecordUpdateDTO implements Serializable {
     private LocalDateTime startTime;
 
     @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "GMT+8")
+    //添加时间格式注解
     private LocalDateTime endTime;
 
     private static final long serialVersionUID = 1L;
